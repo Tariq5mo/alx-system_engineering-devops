1c7f959e4b0f651754a4a9d7f1aa98e7573172d5
Tariq5mo <tariqomer756@gmail.com>
2024-08-11 13:02:27 +0300

    Fix task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,9 +2,9 @@
 """script that, using a REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
-from sys import argv
-from requests import get, request
 import json
+from requests import get, request
+from sys import argv
 
 
 if __name__ == "__main__":
